fc2fd9e74249f2d9f97394216796ee32d1e00e25
hyuntall <hyunchul1998@naver.com>
2022-06-17 23:05:39 +0900

    1629

---
+++ Silver 1/1629번 곱셈.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+A, B, C = map(int, stdin.readline().split())
+
+def solve(A, B):
+    if B == 1:
+        return A % C
+    elif B % 2 == 0:
+        y = solve(A, B//2)
+        return y * y % C
+    else:
+        y = solve(A, (B-1)//2)
+        return y * y * A % C
+
+print(solve(A, B))
